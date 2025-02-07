22b9c089f4ade39d6469208869d72fce772dec5c
ahsan-mahfuz <ahsanmahfuz0@gmail.com>
2025-02-07 14:48:19 +0600

    wait for ui/ux design

--- src/pages/home/userLoginHome/PopularVendorsCategory.jsx
+++ src/pages/home/userLoginHome/PopularVendorsCategory.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import entertainment from '../../../assets/entertainment.jpg'
 import photography from '../../../assets/photography.jpg'
 import venues from '../../../assets/venues.jpg'
@@ -7,66 +9,103 @@ import djs from '../../../assets/djs.jpg'
 import bar from '../../../assets/Bar.jpg'
 import food from '../../../assets/food.jpg'
 
-const images = [
-  { src: entertainment, title: 'Entertainment' },
-  { src: photography, title: 'Photography' },
-  { src: venues, title: 'Venues' },
-  { src: djs, title: 'Djs' },
-  { src: bar, title: 'bar' },
-  { src: food, title: 'food' },
+const TestimonialData = [
+  {
+    id: 1,
+    img: entertainment,
+    name: 'Entertainment',
+  },
+  {
+    id: 2,
+    img: photography,
+    name: 'Photography',
+  },
+  {
+    id: 3,
+    img: venues,
+    name: 'Venues',
+  },
+  {
+    id: 4,
+    img: djs,
+    name: 'DJs',
+  },
+  {
+    id: 5,
+    img: bar,
+    name: 'Bars',
+  },
+  {
+    id: 6,
+    img: food,
+    name: 'Food',
+  },
 ]
 
 const PopularVendorsCategory = () => {
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const itemsPerPage = window.innerWidth >= 768 ? 3 : 1
-  const totalSlides = Math.ceil(images.length / itemsPerPage)
-
-  const prevSlide = () => {
-    setCurrentIndex((prev) => (prev === 0 ? totalSlides - 1 : prev - 1))
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 10000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
-
-  const nextSlide = () => {
-    setCurrentIndex((prev) => (prev === totalSlides - 1 ? 0 : prev + 1))
-  }
-
   return (
-    <div className="responsive-width px-20 max-sm:px-5">
-      <p className="text-3xl font-bold  mb-20">Popular Vandor category</p>
-      <div className="relative w-full max-w-6xl mx-auto overflow-hidden">
-        <div
-          className="flex transition-transform duration-300 ease-in-out"
-          style={{
-            transform: `translateX(-${currentIndex * (100 / itemsPerPage)}%)`,
-            width: `${(images.length / itemsPerPage) * 100}%`,
-          }}
-        >
-          {images.map((image, index) => (
-            <div key={index} className="w-1/3 flex flex-col items-center px-2">
-              <img
-                src={image.src}
-                alt={image.title}
-                className="w-64 h-64 rounded-lg shadow-md object-cover"
-              />
-              <p className="mt-2 text-xl  font-semibold text-center">
-                {image.title}
-              </p>
-            </div>
-          ))}
+    <div className="mt-20">
+      <section className="flex items-center justify-center flex-col">
+        <h1 className="font-bold text-4xl">Popular Vendor Category</h1>
+      </section>
+      <div className="py-10 mb-10">
+        <div className="container mx-auto">
+          <div data-aos="zoom-in">
+            <Slider {...settings}>
+              {TestimonialData.map((data) => (
+                <div className="my-6" key={data.id}>
+                  <div className="flex flex-col gap-4 py-8 px-6 mx-4 rounded-xl  relative">
+                    <div className="mb-4 flex items-center justify-center">
+                      <img
+                        src={data.img}
+                        alt={data.name}
+                        className="w-[400px] h-[300px] object-cover"
+                      />
+                    </div>
+                    <h2 className="text-center text-2xl font-semibold">{data.name}</h2>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
         </div>
-
-        <button
-          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg"
-          onClick={prevSlide}
-        >
-          <FaChevronLeft />
-        </button>
-
-        <button
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg"
-          onClick={nextSlide}
-        >
-          <FaChevronRight />
-        </button>
       </div>
     </div>
   )
